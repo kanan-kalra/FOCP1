af7526d571a67c5dffc1c04c59f36f2735c3412e
kanan kalra <kanan.kalra265@gmail.com>
2025-11-02 16:36:45 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number of scores: ");
+    scanf("%d", &a);
+    int scores[a];
+    int i, found = 0;
+    printf("Enter %d scores:\n", a);
+    for (i = 0; i < a; i++)
+    {
+        scanf("%d", &scores[i]);
+    }
+    for (i = 0; i < a; i++)
+    {
+        if (scores[i] == 99)
+        {
+            printf("The first occurrence of Score 99 is at position %d.\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+    if (found == 0)
+    {
+        printf("Score 99 was not found in the list.\n");
+    }
+
+    return 0;
+}
