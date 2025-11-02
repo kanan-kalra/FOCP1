b90d35defb41c7ba798766a85cb791458bfc369a
kanan kalra <kanan.kalra265@gmail.com>
2025-11-02 16:44:28 +0530

    Update Q11.c

--- Q11.c
+++ Q11.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-                                                                                                                             int main()
+int main()
 {
     int n, i;
     int ecount = 0, ocount = 0;
